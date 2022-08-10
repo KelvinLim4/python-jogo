164335f359813dbed2a9d44784e988f228a53a21
Kelvin Lima <kelvinlimasouza226@gmail.com>
2022-08-10 01:47:05 -0300

    Adicionado condições e comparação de numeros

--- adivinhacao.py
+++ adivinhacao.py
@@ -2,9 +2,20 @@ print("***************************************")
 print("** Bem vindo ao jogo de Adivinhação! **")
 print("***************************************")
 
+#Definindo numero para comparação
 numero_secreto = 42
 
+#Coletando numero do usuario
 chute = input("Digite o seu número: ")
+
+#Convertendo a variavel de string para inteiro
+chute = int(chute)
 
 print("Voce digitou: ", chute)
 
+if (numero_secreto == chute):
+    print("Você acertou!")
+else:
+    print("Você errou!")
+
+print("Fim do Jogo")
