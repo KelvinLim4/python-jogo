a974f703f567aa7992fcfaf1d3677eed0528d744
Kelvin Lima <kelvinlimasouza226@gmail.com>
2022-08-12 22:43:08 -0300

    implementacao do laco while para repeticao de tentativas para acertar o numero secreto

--- adivinhacao.py
+++ adivinhacao.py
@@ -4,24 +4,29 @@ print("***************************************")
 
 #Definindo numero para comparação
 numero_secreto = 42
-#Coletando numero do usuario
-chute = input("Digite o seu número: ")
-#Convertendo a variavel de string para inteiro
-chute = int(chute)
+total_de_tentativas = 3
+rodada = 1
 
-acertou = chute == numero_secreto
-maior   = chute > numero_secreto
-menor   = chute < numero_secreto
+while(rodada <= total_de_tentativas > 0):
+    print("Tentativa {} de {}".format(rodada, total_de_tentativas))
+    chute = input("Digite o seu número: ")
+    chute = int(chute)
 
-print("Voce digitou: ", chute)
+    acertou = chute == numero_secreto
+    maior   = chute > numero_secreto
+    menor   = chute < numero_secreto
 
-if (acertou):
-    print("Você acertou!")
-else:
-    if(maior):
-        print("Você errou! O seu chute foi maior que o numero secreto.")
-    elif(menor):
-        print("Você errou! O seu chute foi menor que o numero secreto.")
+    print("Voce digitou: ", chute)
 
+    if (acertou):
+        print("Você acertou!")
+    else:
+        if(maior):
+            print("Você errou! O seu chute foi maior que o numero secreto.")
+        elif(menor):
+            print("Você errou! O seu chute foi menor que o numero secreto.")
+
+    rodada = rodada + 1
 
 print("Fim do Jogo")
+#saber tipo da variavel type(acertou)
