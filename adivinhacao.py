e194bf0784242fabc38d8dda071626de9acc5d34
Kelvin Lima <kelvinlimasouza226@gmail.com>
2022-08-12 21:44:01 -0300

    armazenando a condicao em variaveis e melhorando a validacao, agora indica se o valor chutado e maior ou menor do que o numero secreto

--- adivinhacao.py
+++ adivinhacao.py
@@ -4,18 +4,24 @@ print("***************************************")
 
 #Definindo numero para comparação
 numero_secreto = 42
-
 #Coletando numero do usuario
 chute = input("Digite o seu número: ")
-
 #Convertendo a variavel de string para inteiro
 chute = int(chute)
 
+acertou = chute == numero_secreto
+maior   = chute > numero_secreto
+menor   = chute < numero_secreto
+
 print("Voce digitou: ", chute)
 
-if (numero_secreto == chute):
+if (acertou):
     print("Você acertou!")
 else:
-    print("Você errou!")
+    if(maior):
+        print("Você errou! O seu chute foi maior que o numero secreto.")
+    elif(menor):
+        print("Você errou! O seu chute foi menor que o numero secreto.")
+
 
 print("Fim do Jogo")
