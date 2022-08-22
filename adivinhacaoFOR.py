2d4784fff0c62db2c0b6d7e7c2ee519704429dd4
Kelvin Lima <kelvinlimasouza226@gmail.com>
2022-08-22 08:51:30 -0300

    Inserção de níveis de dificuldade

--- adivinhacaoFOR.py
+++ adivinhacaoFOR.py
@@ -5,9 +5,20 @@ print("** Bem vindo ao jogo de Adivinhação! **")
 print("***************************************")
 
 #Definindo numero para comparação
-numero_secreto = int(random.random() * 100)
-print(numero_secreto)
-total_de_tentativas = 3
+numero_secreto = random.randrange(1,101)
+total_de_tentativas = 0
+
+print("Qual o nível de dificuldade? ")
+print("(1)Fácil (2)Médio (3)Difícil")
+
+nivel = int(input("Defina o nível: "))
+
+if (nivel == 1):
+    total_de_tentativas = 20
+elif(nivel == 2):
+    total_de_tentativas = 10
+else:
+    total_de_tentativas = 5
 
 for rodada in range (1, total_de_tentativas + 1):
     print("Tentativa {} de {}".format(rodada, total_de_tentativas))
