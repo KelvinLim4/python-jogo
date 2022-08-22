c5353c8e0ca5959a4a07c4073060c7790284c891
Kelvin Lima <kelvinlimasouza226@gmail.com>
2022-08-22 09:06:06 -0300

    Sistema de pontuação implementado

--- adivinhacaoFOR.py
+++ adivinhacaoFOR.py
@@ -7,6 +7,7 @@ print("***************************************")
 #Definindo numero para comparação
 numero_secreto = random.randrange(1,101)
 total_de_tentativas = 0
+pontos = 1000
 
 print("Qual o nível de dificuldade? ")
 print("(1)Fácil (2)Médio (3)Difícil")
@@ -17,8 +18,10 @@ if (nivel == 1):
     total_de_tentativas = 20
 elif(nivel == 2):
     total_de_tentativas = 10
-else:
+elif(numero_secreto == 3):
     total_de_tentativas = 5
+else:
+    print("Digite uma opção valida")
 
 for rodada in range (1, total_de_tentativas + 1):
     print("Tentativa {} de {}".format(rodada, total_de_tentativas))
@@ -36,13 +39,15 @@ for rodada in range (1, total_de_tentativas + 1):
     print("Voce digitou: ", chute)
 
     if (acertou):
-        print("Você acertou!")
+        print("Você acertou e fez {} pontos".format((pontos)))
         break
     else:
         if(maior):
             print("Você errou! O seu chute foi maior que o numero secreto.")
         elif(menor):
             print("Você errou! O seu chute foi menor que o numero secreto.")
+            pontos_perdidos = abs(numero_secreto - chute) #ABS() = número absoluto
+            pontos = pontos - pontos_perdidos
 
 
 print("Fim do Jogo")
