33a8f1eae4db95d0a035e7940cd290fab64812c8
Kelvin Lima <kelvinlimasouza226@gmail.com>
2022-08-28 15:06:57 -0300

    Adcionado vetor para identificar posição das letras acertadas

--- forca.py
+++ forca.py
@@ -4,9 +4,12 @@ def jogar():
     print("***************************************")
 
     palavra_secreta = "banana"
+    letras_acertadas = ["_", "_", "_", "_", "_", "_"]
 
     enforcou = False
     acertou  = False
+
+    print(letras_acertadas)
 
     #while = condição
     #Enquanto nao enforcou e nao acertou = jogue
@@ -16,13 +19,17 @@ def jogar():
         #.strip remove espaços
         chute = chute.strip()
 
+
         index = 0
         #para verificar cada letra dentro da palavra
         for letra in palavra_secreta:
             #.upper Converte os caracteres minúsculos de uma cadeia de caracteres em maiúsculo
             if(chute.upper() == letra.upper()):
-             print("Encontrei {} na posição {}".format(letra, index))
+                #alimentando o vetor
+                letras_acertadas[index] = letra
             index = index + 1
+
+        print(letras_acertadas)
 
         print("Jogando ...")
     print("Fim do Jogo")
