08e7b9865350b0252d7aa8e30704ad21afa5a614
Kelvin Lima <kelvinlimasouza226@gmail.com>
2022-08-28 13:32:10 -0300

    Adicionado tratamento de dados inseridos pelo usuario

--- forca.py
+++ forca.py
@@ -8,18 +8,23 @@ def jogar():
     enforcou = False
     acertou  = False
 
+    #while = condição
+    #Enquanto nao enforcou e nao acertou = jogue
     while(not enforcou and not acertou):
 
         chute = input("Qual a letra?")
+        #.strip remove espaços
+        chute = chute.strip()
 
         index = 0
+        #para verificar cada letra dentro da palavra
         for letra in palavra_secreta:
-            if(chute == letra):
+            #.upper Converte os caracteres minúsculos de uma cadeia de caracteres em maiúsculo
+            if(chute.upper() == letra.upper()):
              print("Encontrei {} na posição {}".format(letra, index))
             index = index + 1
 
         print("Jogando ...")
-
     print("Fim do Jogo")
 
 # Variavel interna para execução primaria
